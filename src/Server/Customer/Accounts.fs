37b3979fa528857db82ecb7b45b71b894e7e4190
cotyar <khabarov@gmail.com>
2018-06-27 22:45:52 +0100

    Btc keys generation corrected

--- src/Server/Customer/Accounts.fs
+++ src/Server/Customer/Accounts.fs
@@ -12,25 +12,31 @@ type PublicAccountPart = {
 type PrivateAccountPart = {
     CustomerId: System.Guid
     EthAddress: string
-    EthKey: string
+    EthPubKey:  string
+    EthPrivKey: string
     BtcAddress: string
-    BtcKey: string
+    BtcPubKey:  string
+    BtcPrivKey: string
 }
 
 
 let masterEncryptionKey = "<GetFromConfig>" // Load from configuration 
 
 let createCustomerAccounts id = 
-    let ethEcKey = Signer.EthECKey.GenerateKey()
-    let ethPrivateKey = ethEcKey.GetPrivateKeyAsBytes().ToHex()  // TODO: Encrypt
+    let ethEcKey      = Signer.EthECKey.GenerateKey()
+    let ethPubKey     = ethEcKey.GetPubKey().ToHex()  // TODO: Encrypt
+    let ethPrivateKey = ethEcKey.GetPrivateKeyAsBytes().ToHex()
     
-    let btcKey          = new NBitcoin.Key()
-    let btcAddress      = btcKey.PubKey.GetAddress(NBitcoin.Network.TestNet)
-    let btcPrivateKey   = btcKey.GetBitcoinSecret(NBitcoin.Network.TestNet)
+    let btcKey          = new NBitcoin.ExtKey()
+    let btcAddress      = btcKey.PrivateKey.PubKey.GetAddress(NBitcoin.Network.TestNet).ToString()
+    let btcPubKey       = btcKey.GetWif(NBitcoin.Network.TestNet).ToString()
+    let btcPrivateKey   = btcKey.PrivateKey.GetWif(NBitcoin.Network.TestNet).ToString()
 
     {   CustomerId  = id
         EthAddress  = ethEcKey.GetPublicAddress()
-        EthKey      = ethPrivateKey
-        BtcAddress  = "" //btcAddress.ToString()
-        BtcKey      = "" 
+        EthPubKey   = ethPubKey
+        EthPrivKey  = ethPrivateKey
+        BtcAddress  = btcAddress
+        BtcPubKey   = btcPubKey
+        BtcPrivKey  = btcPrivateKey 
     }
